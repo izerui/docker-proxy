e61385ab8eb7b962d8b844d8f8dab554b21ae3ba
serv <liuyuhua69@gmail.com>
2024-10-30 20:56:34 +0800

    change2

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-docker build -f Dockerfile -t docker-proxy:1.3 .
-docker tag docker-proxy:1.3 izerui/docker-proxy:1.3
-docker push izerui/docker-proxy:1.3
+docker build -f Dockerfile -t docker-proxy:1.4 .
+docker tag docker-proxy:1.4 izerui/docker-proxy:1.4
+docker push izerui/docker-proxy:1.4
